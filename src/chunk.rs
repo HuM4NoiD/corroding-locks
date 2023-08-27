8a7097b43e3918faf211e8146ef54a0088cee21e
Jugal Mistry <jugalmistry99@gmail.com>
2023-08-27 22:52:26 +0530

    Add get_line method to get line number for bytecode index, use u32 for line numbers

--- src/chunk.rs
+++ src/chunk.rs
@@ -12,7 +12,7 @@ pub enum OpCode {
 #[derive(Debug)]
 pub struct Chunk {
     pub code: Vec<u8>,
-    pub lines: Vec<i32>,
+    pub lines: Vec<u32>,
     pub value_array: VmValueArray,
 }
 
@@ -21,7 +21,7 @@ impl Chunk {
         Chunk { code: vec![], lines: vec![], value_array: VmValueArray::new() }
     }
 
-    pub fn write(&mut self, byte: u8, line: i32) {
+    pub fn write(&mut self, byte: u8, line: u32) {
         self.code.push(byte);
         self.lines.push(line);
     }
@@ -33,5 +33,13 @@ impl Chunk {
     pub fn add_constant(&mut self, value: VmValue) -> usize {
         self.value_array.add(value);
         self.value_array.values.len() - 1
+    }
+
+    pub fn get_line(&self, chunk_index: usize) -> Option<u32> {
+        if self.lines.len() <= chunk_index {
+            None
+        } else {
+            Some(self.lines[chunk_index])
+        }
     }
 }
